2c34ac76cdcb5c79178c1697707dc59950b0f5ff
hacpaka <hacpaka@eggbe.com>
2018-09-28 01:34:42 +0300

    +

--- src/main.php
+++ src/main.php
@@ -11,3 +11,25 @@ if (!function_exists('preg_count')) {
 		return $count;
 	}
 }
+
+if (!function_exists('array_merge_recursive_distinct')) {
+
+	/**
+	 * @param array $Original
+	 * @param array $Source
+	 * @return array
+	 */
+	function array_merge_recursive_distinct(array $Original, array $Source) {
+		foreach ($Source as $key => $value) {
+			if (is_array($value) && isset($Original[$key]) && is_array($Original[$key])) {
+				$Original[$key] = array_merge_recursive_distinct($Original[$key], $value);
+			} elseif (is_numeric($key)) {
+				array_push($Original, $value);
+			} else {
+				$Original[$key] = $value;
+			}
+		}
+
+		return $Original;
+	}
+}
